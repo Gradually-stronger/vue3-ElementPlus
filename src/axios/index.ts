33f363201775083d1bb497615c69a66f64bf3823
juvenile <1050299953@qq.com>
2022-06-07 13:18:05 +0800

    uri changed

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ElLoading, ElNotification } from "element-plus";
+import { ElNotification } from "element-plus";
 import { AxiosRequestConfig, Method } from "axios";
 
 
