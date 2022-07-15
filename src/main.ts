22de21aa5377163f7c7044228679edff764c1c48
juvenile <1050299953@qq.com>
2022-07-15 10:49:21 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router/index';
-import { store } from './vuex/index';
-import Particles from 'particles.vue3';
 import 'element-plus/dist/index.css';
 import './index.css';
 import 'element-plus/theme-chalk/dark/css-vars.css';
 import '@wangeditor/editor/dist/css/style.css'
+
+import App from './App.vue';
+import router from './router/index';
+import { store } from './vuex/index';
+import Particles from 'particles.vue3';
 /**
  * //挂载全局 app.config.globalProperties.$ELEMENT = option
  * import { getCurrentInstance} from 'vue';
